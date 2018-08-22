f4c3d0397f15b9a9401322ef8211519d2ddcb047
Adrian <adrian@netkata.com>
2018-08-22 16:03:52 +0200

    seconds

--- scripts.js
+++ scripts.js
@@ -3,10 +3,39 @@ var hours = currentTime.getHours();
 var minutes = currentTime.getMinutes();
 var seconds = currentTime.getSeconds();
 
+
 console.log('hours: ' + hours);
 console.log('minutes: ' + minutes);
 console.log('seconds: ' + seconds);
 
-function getDeg() {
-    return 360/60;
-}
+var setSeconds = 6*seconds+90;
+document.querySelector('.seconds').style.transform = 'rotate('+setSeconds+'deg)';
+console.log('deg:' +setSeconds);
+
+
+var startSeconds = setInterval(function(){
+        setSeconds += 6;
+        console.log('deg:' +setSeconds);
+        document.querySelector('.seconds').style.transform = 'rotate('+setSeconds+'deg)';
+    // }
+}, 1000);
+
+
+//
+// function goSeconds () {
+//     for (var i = 0; i < setSeconds; i++){
+//         if setSeconds
+//     }
+// }
+
+
+// document.querySelector('.seconds').style.transform = 'rotate('+90+'deg)';
+
+
+//1. określenie kąta wskazówki w stosunku do pobranej wartości
+//a. ustawienie wartości 0 dla wskazówek rotate(90) daje 12:00
+//2. iteracja wskazówek
+//3. zależność między wskazówkami
+
+/* koło ma 360 stopni, sekunda robi koło w 60 sekund, każdy jej skok daje 6 stopni. Obracanie sekundnika musi
+* */
