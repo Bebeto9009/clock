cba357bf070e179d0bd1e5a6bc9354f565e831bf
bebeto9009 <adr.pawlak@gmail.com>
2018-08-23 20:40:35 +0200

    final clock

--- scripts.js
+++ scripts.js
@@ -2,23 +2,34 @@ var currentTime = new Date();
 var hour = currentTime.getHours();
 var minute = currentTime.getMinutes();
 var second = currentTime.getSeconds();
-// var second = 15;
 
-var hourAngle=0,
-    minuteAngle=0,
-    secondAngle=0;
+var hourAngle = (360/12),
+    minuteAngle = (360/60),
+    secondAngle = (360/60),
+    setSecond = second*secondAngle,
+    setMinute = minute*minuteAngle, //30*6=180
+    setHour = hour*hourAngle+(setMinute/12); //9*30+180/12=270+15=285
 
-var setSecond;
+document.querySelector('.seconds').style.transform = 'rotate(' +setSecond+ 'deg)';
+document.querySelector('.minutes').style.transform = 'rotate(' +setMinute+ 'deg)';
+document.querySelector('.hours').style.transform = 'rotate(' +setHour+ 'deg)';
 
 function getAngle() {
-    // hourAngle = hour
-    // minuteAngle =
-    secondAngle = 360/60;
-    setSecond = second * secondAngle
+    setSecond += 6;
     document.querySelector('.seconds').style.transform = 'rotate(' +setSecond+ 'deg)';
     console.log('second angle: ' + setSecond);
+
+    setMinute += 6/60;
+    document.querySelector('.minutes').style.transform = 'rotate(' +setMinute+ 'deg)';
+    console.log('minute angle: ' + setMinute);
+
+    setHour += 6/60/12;
+    document.querySelector('.hours').style.transform = 'rotate(' +setHour+ 'deg)';
+    console.log('hours angle: ' + setHour);
 }
 
 setInterval(function() {
     getAngle();
 }, 1000);
+
+
