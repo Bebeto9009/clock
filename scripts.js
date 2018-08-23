bd4fb8812bf3b1405152ddbfee6f0e6de8b42719
bebeto9009 <adr.pawlak@gmail.com>
2018-08-23 20:40:53 +0200

    final clock

--- scripts.js
+++ scripts.js
@@ -7,8 +7,8 @@ var hourAngle = (360/12),
     minuteAngle = (360/60),
     secondAngle = (360/60),
     setSecond = second*secondAngle,
-    setMinute = minute*minuteAngle, //30*6=180
-    setHour = hour*hourAngle+(setMinute/12); //9*30+180/12=270+15=285
+    setMinute = minute*minuteAngle,
+    setHour = hour*hourAngle+(setMinute/12);
 
 document.querySelector('.seconds').style.transform = 'rotate(' +setSecond+ 'deg)';
 document.querySelector('.minutes').style.transform = 'rotate(' +setMinute+ 'deg)';
