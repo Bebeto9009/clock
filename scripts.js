60cf35ef82dee3694b3fa88703bd59d5ca55529d
Adrian <adrian@netkata.com>
2018-08-27 13:50:29 +0200

    bem

--- scripts.js
+++ scripts.js
@@ -1,28 +1,29 @@
-var currentTime = new Date();
-var hour = currentTime.getHours();
-var minute = currentTime.getMinutes();
-var second = currentTime.getSeconds();
-
-var hourAngle = (360/12),
-    minuteAngle = (360/60),
-    secondAngle = (360/60),
-    setSecond = second*secondAngle,
-    setMinute = minute*minuteAngle+(setSecond/60),
-    setHour = hour*hourAngle+(setMinute/12);
-
-document.querySelector('.seconds').style.transform = 'rotate(' +setSecond+ 'deg)';
-document.querySelector('.minutes').style.transform = 'rotate(' +setMinute+ 'deg)';
-document.querySelector('.hours').style.transform = 'rotate(' +setHour+ 'deg)';
-
 function getAngle() {
+    var currentTime = new Date();
+
+    var hour = currentTime.getHours();
+    var minute = currentTime.getMinutes();
+    var second = currentTime.getSeconds();
+
+    var hourAngle = (360/12),
+        minuteAngle = (360/60),
+        secondAngle = (360/60),
+        setSecond = second*secondAngle,
+        setMinute = minute*minuteAngle+(setSecond/60),
+        setHour = hour*hourAngle+(setMinute/12);
+
+    document.querySelector('.circle__seconds').style.transform = 'rotate(' +setSecond+ 'deg)';
+    document.querySelector('.circle__minutes').style.transform = 'rotate(' +setMinute+ 'deg)';
+    document.querySelector('.circle__hours').style.transform = 'rotate(' +setHour+ 'deg)';
+
     setSecond += 6;
-    document.querySelector('.seconds').style.transform = 'rotate(' +setSecond+ 'deg)';
+    document.querySelector('.circle__seconds').style.transform = 'rotate(' +setSecond+ 'deg)';
 
     setMinute += 6/60;
-    document.querySelector('.minutes').style.transform = 'rotate(' +setMinute+ 'deg)';
+    document.querySelector('.circle__minutes').style.transform = 'rotate(' +setMinute+ 'deg)';
 
     setHour += 6/60/12;
-    document.querySelector('.hours').style.transform = 'rotate(' +setHour+ 'deg)';
+    document.querySelector('.circle__hours').style.transform = 'rotate(' +setHour+ 'deg)';
 }
 
 setInterval(function() {
