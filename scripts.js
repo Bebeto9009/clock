2b21019c582ab65c7c30295a00bede6aea3cac66
Adrian <adrian@netkata.com>
2018-08-27 14:13:51 +0200

    optimization

--- scripts.js
+++ scripts.js
@@ -14,13 +14,8 @@ function clock() {
         setMinute = minute*minuteAngle+(setSecond/60),
         setHour = hour*hourAngle+(setMinute/12);
 
-    setSecond += 6;
     document.querySelector('.circle__seconds').style.transform = 'rotate(' +setSecond+ 'deg)';
-
-    setMinute += 6/60;
     document.querySelector('.circle__minutes').style.transform = 'rotate(' +setMinute+ 'deg)';
-
-    setHour += 6/60/12;
     document.querySelector('.circle__hours').style.transform = 'rotate(' +setHour+ 'deg)';
 }
 
